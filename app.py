455d66a4d1c6e2273db925cb113717ff8fb076c6
zachchao <zach.chao@yahoo.com>
2018-10-26 03:53:07 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask
+from flask import render_template, request
+from flask_sslify import SSLify
+
+
+app = Flask(__name__)
+sslify = SSLify(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/robots.txt')
+def robots():
+    res = app.make_response('User-agent: *\nAllow: /')
+    res.mimetype = 'text/plain'
+    return res
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
